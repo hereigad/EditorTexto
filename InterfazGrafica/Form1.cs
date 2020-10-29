7a8b1a12b4e3d32c11a8bb79cb6f2ea359d7994b
hereigad <hectormetroid@gmail.com>
2020-10-29 11:22:02 +0100

    prueba

--- InterfazGrafica/Form1.cs
+++ InterfazGrafica/Form1.cs
@@ -24,7 +24,7 @@ namespace InterfazGrafica
 
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            
         }
     }
 }
